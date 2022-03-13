be02a634bbab6d73c982ad09ad1df393869370ac
PlusHistoriae <85767310+PlusHistoriae@users.noreply.github.com>
2022-03-13 20:49:30 +0000

    Various Huge Changes

--- main.py
+++ main.py
@@ -1,44 +1,59 @@
-      #PACKAGES/MODULES
-
 import matplotlib.pyplot as plt
 import random as rdm
+import numpy as np
 
       #VARTIABLRES
+
+days = 10
+
+species_population = 15
+predator_population = 10
+tree_population = 50
+
+trees = np.arange(tree_population)
+predators = np.arange(predator_population)
+species = np.arange(species_population)
+
+predator_assignment = rdm.sample(list(trees), predator_population)
 
 species_data = []
 predator_data = []
 tree_data = []
 
-days = int(input("How many days should the simulation run for?     "))
-print()
-species_population = int(input("How many of the species would you like?    "))
-print()
-predator_population = int(input("How many of the predators would you like?    "))
-print()
-tree_population = int(input("How many of the trees would you like?    "))
-print()
-
-apples = tree_population*2
-
       #CODE
 
+print(predator_assignment)
 for simulation in range(days):
-  apples = int(apples - species_population)
-  species_population = species_population*apples
-  apples = int(tree_population*2)
-  round(species_population)
+  # assign blobs to trees
+  species_assignment = rdm.sample(list(trees), species_population)
+
+  # remove bloos that went to trees with predators
+  for i in range(species_population):
+    if species_assignment[i] in set(predator_assignment):
+      species_population = int(species_population - 1)
+
+  # blobs that survived reproduce
+
+  for reproduce in range(species_population):
+    species_population = species_population + rdm.randint(1,2)
+
+  if species_population >= tree_population:
+    species_population = 5
+
+  # appending data to graph
+  
+  species_data.append(species_population)
+  predator_data.append(predator_population)     
+  tree_data.append(tree_population)
   print(species_population)
 
-  #Plotting the Data on a Graph (uses matplotlib)
   
-  species_data.append(species_population)  #Species
-  plt.plot(species_data, label = "Species")
-  predator_data.append(predator_population)     #Predators
-  plt.plot(predator_data, label = "Predator")
-  tree_data.append(tree_population)     #Trees
-  plt.plot(tree_data, label = "Trees")
 
 Title = input("What is the name of the Graph?      ")
+
+plt.plot(species_data, label = "Species")
+plt.plot(predator_data, label = "Predator")
+plt.plot(tree_data, label = "Trees")
 
 plt.title(Title)
 plt.legend()
